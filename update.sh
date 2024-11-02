f2b222225933e7cb5b5bac3291cf735b556243dd
s1522711 <s1522711@gmail.com>
2024-11-02 20:12:30 +0200

    added a quick update script

---
+++ update.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+# stop the bot
+pm2 stop "ovixTicketKillerBot"
+# pull the latest changes
+git pull
+# install the latest dependencies
+npm install
+# start the bot
+pm2 start npm --name "ovixTicketKillerBot" -- start
