998859fbcc279702e76ec4f80920c719491b909d
s1522711 <52498188+s1522711@users.noreply.github.com>
2025-09-25 19:11:03 +0300

    Add permission check for randomize-code command
    Added permission check for Administrator role before executing the command.

--- commands/tickets/randomize-code.js
+++ commands/tickets/randomize-code.js
@@ -8,6 +8,9 @@ module.exports = {
 		.setName('randomize-code')
 		.setDescription('randomize the ticket code'),
 	async execute(interaction) {
+		if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+			return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+		}
 		await interaction.deferReply();
 		await randomizeCode(interaction.client);
 		interaction.editReply(`Code randomized. New code: \`${state.killing.ticketCode}\``);
